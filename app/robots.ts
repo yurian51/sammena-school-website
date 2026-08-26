e7f1e277eaaa222edfd6595a06a0c4ec0b032526
yurian51 <yurianarshad@gmail.com>
2026-08-26 11:16:50 +0300

    seo: add robots metadata route

---
+++ app/robots.ts
@@ -0,0 +1,10 @@
+import type { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL ?? 'https://sammena.sch.tz'
+  return {
+    rules: [{ userAgent: '*', allow: '/' }],
+    sitemap: `${baseUrl}/sitemap.xml`,
+    host: baseUrl,
+  }
+}
