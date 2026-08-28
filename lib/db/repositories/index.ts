eaf1f11a09b07ea819a90e7763828c7370e7c6ce
yurian51 <yurianarshad@gmail.com>
2026-08-28 13:18:05 +0300

    feat(db): export student postgres repository

--- lib/db/repositories/index.ts
+++ lib/db/repositories/index.ts
@@ -1,3 +1,4 @@
 export { PostgresAdmissionsRepository } from "./admissions-postgres"
 export { PostgresCmsRepository } from "./cms-postgres"
 export { PostgresAuditRepository } from "./audit-postgres"
+export { PostgresStudentRepository } from "./student-postgres"
