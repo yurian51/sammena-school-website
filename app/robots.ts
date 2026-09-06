42a344c1b5674fd60abc57a2005a2ca5bf930d82
yurian51 <yurianarshad@gmail.com>
2026-09-06 13:22:07 +0300

    chore: tighten crawl policy

--- app/robots.ts
+++ app/robots.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from 'next'
 export default function robots(): MetadataRoute.Robots {
   const baseUrl = process.env.NEXT_PUBLIC_SITE_URL ?? 'https://sammena.sch.tz'
   return {
-    rules: [{ userAgent: '*', allow: '/' }],
+    rules: [{ userAgent: '*', allow: '/', disallow: ['/api/', '/portal/'] }],
     sitemap: `${baseUrl}/sitemap.xml`,
     host: baseUrl,
   }
